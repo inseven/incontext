b8399e18cc753bbe45707a50c3bead83a6043def
Jason Morley <hello@jbmorley.co.uk>
2023-07-19 20:26:37 -1000

    feat: Basic video conversion

--- Sources/Importers/VideoImporter.swift
+++ Sources/Importers/VideoImporter.swift
@@ -20,16 +20,63 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import AVFoundation
 import Foundation
 
 class VideoImporter: Importer {
 
     let identifier = "app.incontext.importer.video"
     let legacyIdentifier = "import_video"
-    let version = 1
+    let version = 2
 
     func process(site: Site, file: File, settings: [AnyHashable: Any]) async throws -> ImporterResult {
+        let video = AVAsset(url: file.url)
+        let outputURL = site.outputURL(relativePath: file.url.deletingPathExtension().relativePath + ".mov")
+        try FileManager.default.createDirectory(at: outputURL.deletingLastPathComponent(),
+                                                withIntermediateDirectories: true)
+        await export(video: video,
+                     withPreset: AVAssetExportPresetLowQuality,
+                     toFileType: .mov,
+                     atURL: outputURL)
+
+//        let newAsset = AVAsset(url:Bundle.main.url(forResource: "jumping-man", withExtension: "mov")!) //1
+//        var newSize = <some size that you've calculated> //2
+//        let resizeComposition = AVMutableVideoComposition(asset: newAsset, applyingCIFiltersWithHandler: { request in
+//          let filter = CIFilter(name: "CILanczosScaleTransform") //3
+//          filter?.setValue(request.sourceImage, forKey: kCIInputImageKey)
+//          filter?.setValue(<some scale factor>, forKey: kCIInputScaleKey) //4
+//          let resultImage = filter?.outputImage
+//          request.finish(with: resultImage, context: nil)
+//        })
+//        resizeComposition.renderSize = newSize //5
+
         return ImporterResult()
+    }
+
+    func export(video: AVAsset,
+                withPreset preset: String = AVAssetExportPresetHighestQuality,
+                toFileType outputFileType: AVFileType = .mov,
+                atURL outputURL: URL) async {
+
+        // Check the compatibility of the preset to export the video to the output file type.
+        guard await AVAssetExportSession.compatibility(ofExportPreset: preset,
+                                                       with: video,
+                                                       outputFileType: outputFileType) else {
+            print("The preset can't export the video to the output file type.")
+            return
+        }
+
+        // Create and configure the export session.
+        guard let exportSession = AVAssetExportSession(asset: video,
+                                                       presetName: preset) else {
+            print("Failed to create export session.")
+            return
+        }
+        exportSession.outputFileType = outputFileType
+        exportSession.outputURL = outputURL
+
+        // Convert the video to the output file type and export it to the output URL.
+        await exportSession.export()
     }
 
 }
