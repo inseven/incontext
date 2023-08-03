3b9cc92d77a32796c21b5a9463c4437211d549b5
Jason Morley <hello@jbmorley.co.uk>
2023-08-02 19:59:31 -1000

    Remove stencil and identity template languages

--- Tests/ic3tests/HandlerTests.swift
+++ Tests/ic3tests/HandlerTests.swift
@@ -31,7 +31,7 @@ class HandlerTests: XCTestCase {
         let handler: Handler<MarkdownImporter> = try .init(when: "photos.*\\.(jpg|jpeg|png|gif|tiff|heic)",
                                                            importer: MarkdownImporter(),
                                                            settings: .init(defaultCategory: "general",
-                                                                           defaultTemplate: .stencil("posts.html")))
+                                                                           defaultTemplate: .tilt("posts.html")))
         XCTAssertTrue(try handler.matches(relativePath: "photos/snapshots/image.heic"))
         XCTAssertTrue(try handler.matches(relativePath: "photos/snapshots/image.HEIC"))
     }
@@ -40,18 +40,18 @@ class HandlerTests: XCTestCase {
         let handler1: Handler<MarkdownImporter> = try .init(when: "photos.*\\.(jpg|jpeg|png|gif|tiff|heic)",
                                                             importer: MarkdownImporter(),
                                                             settings: .init(defaultCategory: "general",
-                                                                            defaultTemplate: .stencil("posts.html")))
+                                                                            defaultTemplate: .tilt("posts.html")))
 
         let handler2: Handler<MarkdownImporter> = try .init(when: "photos.*\\.(jpg|jpeg|png|gif|tiff|heic)",
                                                             importer: MarkdownImporter(),
                                                             settings: .init(defaultCategory: "general",
-                                                                            defaultTemplate: .stencil("posts.html")))
+                                                                            defaultTemplate: .tilt("posts.html")))
         XCTAssertEqual(try handler1.fingerprint(), try handler2.fingerprint())
 
         let handler3: Handler<MarkdownImporter> = try .init(when: "photos.*\\.(jpg|jpeg|png|gif|tiff|heic)",
                                                             importer: MarkdownImporter(),
                                                             settings: .init(defaultCategory: "photos",
-                                                                            defaultTemplate: .stencil("posts.html")))
+                                                                            defaultTemplate: .tilt("posts.html")))
         XCTAssertNotEqual(try handler1.fingerprint(), try handler3.fingerprint())
     }
 
@@ -59,8 +59,8 @@ class HandlerTests: XCTestCase {
         let handler: Handler<MarkdownImporter> = try .init(when: "photos.*\\.(jpg|jpeg|png|gif|tiff|heic)",
                                                            importer: MarkdownImporter(),
                                                            settings: .init(defaultCategory: "general",
-                                                                           defaultTemplate: .stencil("posts.html")))
-        XCTAssertEqual(try handler.fingerprint(), "fXY89pkCFPfqXvGLrAOQ6w==")
+                                                                           defaultTemplate: .tilt("posts.html")))
+        XCTAssertEqual(try handler.fingerprint(), "UtnhV0oG8R7Xzn4sz0IMoA==")
     }
 
 }
