5b293a7b6cecc6893a371af117d154529af7e1b5
Jason Morley <hello@jbmorley.co.uk>
2023-08-03 13:03:36 -1000

    ci: Add script to build and run

---
+++ Scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIRECTORY="${SCRIPTS_DIRECTORY}/.."
+
+pushd "$ROOT_DIRECTORY"
+swift build
+BINARY_DIRECTORY=`swift build --product incontext --show-bin-path`
+BINARY_DIRECTORY="$( cd "$BINARY_DIRECTORY" &> /dev/null &&
+pwd )"
+popd
+
+"$BINARY_DIRECTORY/incontext" $@
