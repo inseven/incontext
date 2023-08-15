c72d92132dc8f960254971144c187cfde075690c
Jason Morley <hello@jbmorley.co.uk>
2023-08-14 19:22:07 -1000

    ci: Use make and lipo to generate fat binaries

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIRECTORY="${SCRIPTS_DIRECTORY}/.."
+
+pushd "$ROOT_DIRECTORY"
+
 swift build
-swift build --product incontext --show-bin-path
+swift test
+
+make clean
+make release
