c33d87685aaf61bc3412aa26696a7c5941749fee
Jason Morley <hello@jbmorley.co.uk>
2023-08-26 18:23:59 -1000

    fix: Initial work to add a menu bar helper (#125)

---
+++ InContext/Helper/Geocoder.swift
@@ -0,0 +1,48 @@
+// MIT License
+//
+// Copyright (c) 2023 Jason Barrie Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import CoreLocation
+import Foundation
+
+class Geocoder {
+
+    static func coordinate(for address: String) async throws -> CLLocationCoordinate2D {
+        let geocoder = CLGeocoder()
+        let placemarks = try await geocoder.geocodeAddressString(address)
+        let locations = placemarks.compactMap { $0.location }
+        guard let location = locations.first else {
+            throw HelperError.general("Failed to find a location for address '\(address)'.")
+        }
+        return location.coordinate
+    }
+
+    static func xLocation(for address: String) async throws -> String {
+        let coordinate = try await coordinate(for: address)
+        let location = """
+<x-location latitude="\(coordinate.latitude)" longitude="\(coordinate.longitude)">
+    \(address)
+</x-location>
+"""
+        return location.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+}
