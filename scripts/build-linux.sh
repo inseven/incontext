43f153ef4239723155c9f3d7c420711f80c7cee6
Jason Morley <hello@jbmorley.co.uk>
2026-07-09 10:50:07 -1000

    feat: 📦 Create Debian packages (#355)

---
+++ scripts/build-linux.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2016-2026 Jason Morley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+set -o pipefail
+set -x
+set -u
+
+function fatal {
+    echo $1 >&2
+    exit 1
+}
+
+ROOT_DIRECTORY="$( cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
+BUILD_DIRECTORY="$ROOT_DIRECTORY/build"
+SWIFT_BUILD_DIRECTORY="$ROOT_DIRECTORY/.build"
+ARTIFACTS_DIRECTORY="$BUILD_DIRECTORY/artifacts"
+
+# Clean up and recreate the output directories.
+if [ -d "$SWIFT_BUILD_DIRECTORY" ] ; then
+    rm -rf "$SWIFT_BUILD_DIRECTORY"
+fi
+if [ -d "$BUILD_DIRECTORY" ] ; then
+    rm -r "$BUILD_DIRECTORY"
+fi
+mkdir -p "$BUILD_DIRECTORY"
+mkdir -p "$ARTIFACTS_DIRECTORY"
+
+# Determine the version and build number.
+# We expect these to be injected in by our GitHub build job so we just ensure there are sensible defaults.
+VERSION_NUMBER=${VERSION_NUMBER:-0.0.0}
+BUILD_NUMBER=${BUILD_NUMBER:-0}
+
+# Log the Swift version.
+swift --version
+
+# Run the tests.
+swift test
+
+# Build the project (debug and release).
+swift build -Xcc "-DVERSION_NUMBER=\"$VERSION_NUMBER\"" -Xcc "-DBUILD_NUMBER=\"$BUILD_NUMBER\""
+swift build \
+    -c release \
+    -Xcc "-DVERSION_NUMBER=\"$VERSION_NUMBER\"" -Xcc "-DBUILD_NUMBER=\"$BUILD_NUMBER\"" \
+    -Xswiftc -static-stdlib
+
+# Ensure the commands have been created and can run.
+"$SWIFT_BUILD_DIRECTORY/debug/incontext" --version
+"$SWIFT_BUILD_DIRECTORY/release/incontext" --version
+
+cd "$BUILD_DIRECTORY"
+
+# Package.
+source /etc/os-release
+DISTRO=$ID
+DESCRIPTION="Multimedia-focused static site builder"
+URL="https://incontext.jbmorley.co.uk"
+MAINTAINER="Jason Morley <support@jbmorley.co.uk>"
+
+case $DISTRO in
+    ubuntu|debian)
+
+        ARCHITECTURE=`dpkg --print-architecture`
+        PACKAGE_FILENAME="incontext.deb"
+        fpm \
+            -s dir \
+            -t deb \
+            -p "$PACKAGE_FILENAME" \
+            --name "incontext" \
+            --version "${VERSION_NUMBER}~${VERSION_CODENAME}${BUILD_NUMBER}" \
+            --architecture "$ARCHITECTURE" \
+            --description "$DESCRIPTION" \
+            --url "$URL" \
+            --maintainer "$MAINTAINER" \
+            --depends libsqlite3-0 \
+            "$SWIFT_BUILD_DIRECTORY/release/incontext=/usr/bin/incontext"
+        ;;
+
+    *)
+        fatal "Error: Unsupported distribution: $DISTRO."
+        ;;
+esac
+
+# Copy the release binary to the artifacts directory.
+cp "$PACKAGE_FILENAME" "$ARTIFACTS_DIRECTORY"
---
+++ scripts/install-linux-dependencies.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2016-2026 Jason Morley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+set -o pipefail
+set -x
+set -u
+
+function fatal {
+    echo $1 >&2
+    exit 1
+}
+
+# $USER is unbound in GitHub Actions containers. We check this as a way of inferring that we shouldn't be using sudo.
+if [ -z ${USER+x} ]; then
+    SUDO=""
+else
+    SUDO="sudo"
+fi
+
+# Install the per-platform build dependencies.
+source /etc/os-release
+case $ID in
+    ubuntu|debian)
+        $SUDO apt-get update -y
+        $SUDO apt-get install -y \
+            build-essential git \
+            ruby ruby-bundler \
+            libsqlite3-dev
+        $SUDO gem install --no-user-install fpm
+        ;;
+
+    *)
+        fatal "Error: Unsupported distribution: $ID."
+        ;;
+esac
