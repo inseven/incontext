6ac0e6c82d932e5ccd802e2ae42f2d3748b4ec16
Jason Morley <hello@jbmorley.co.uk>
2026-07-16 14:36:13 -1000

    Cleaner

---
+++ Sources/InContextCore/Model/FileType.swift
@@ -0,0 +1,92 @@
+// MIT License
+//
+// Copyright (c) 2016-2026 Jason Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+enum FileType: String, CaseIterable {
+    case gif = "image/gif"
+    case heic = "image/heic"
+    case html = "text/html"
+    case jpeg = "image/jpeg"
+    case markdown = "text/markdown"
+    case quickTimeVideo = "video/quicktime"
+    case tiff = "image/tiff"
+}
+
+extension FileType {
+
+    internal enum Extension: String, CaseIterable {
+        case gif
+        case heic
+        case htm
+        case html
+        case jpeg
+        case jpg
+        case markdown
+        case md
+        case mov
+        case tiff
+
+        var fileType: FileType {
+            switch self {
+            case .gif: .gif
+            case .heic: .heic
+            case .htm, .html: .html
+            case .jpeg, .jpg: .jpeg
+            case .markdown, .md: .markdown
+            case .mov: .quickTimeVideo
+            case .tiff: .tiff
+            }
+        }
+    }
+
+    var preferredFilenameExtension: String? {
+        func map(_ fileType: FileType) -> Extension {
+            switch fileType {
+            case .gif: .gif
+            case .heic: .heic
+            case .html: .html
+            case .jpeg: .jpeg
+            case .markdown: .md
+            case .quickTimeVideo: .mov
+            case .tiff: .tiff
+            }
+        }
+        return map(self).rawValue
+    }
+
+    var preferredMIMEType: String? {
+        return self.rawValue
+    }
+
+    init?(mimeType: String) {
+        self.init(rawValue: mimeType.lowercased())
+    }
+
+    init?(filenameExtension: String) {
+        guard let filenameExtension = Extension(rawValue: filenameExtension.lowercased()) else {
+            return nil
+        }
+        self = filenameExtension.fileType
+    }
+
+}
--- Tests/InContextTests/Tests/UTTypeTests.swift
+++ Tests/InContextTests/Tests/UTTypeTests.swift
@@ -27,94 +27,6 @@ import XCTest
 
 import PlatformSupport
 
-enum FileType: String, CaseIterable {
-    case gif = "image/gif"
-    case heic = "image/heic"
-    case html = "text/html"
-    case jpeg = "image/jpeg"
-    case markdown = "text/markdown"
-    case quickTimeVideo = "video/quicktime"
-    case tiff = "image/tiff"
-}
-
-extension FileType {
-
-    // TODO: Rename to Extension
-    // TODO: Make this private again.
-    fileprivate enum FilenameExtension: String, CaseIterable {
-        case gif
-        case heic
-        case htm
-        case html
-        case jpeg
-        case jpg
-        case markdown
-        case md
-        case mov
-        case tiff
-    }
-
-    private static func extensions(for fileType: FileType) -> [FilenameExtension] {
-        switch fileType {
-        case .gif:
-            return [.gif]
-        case .heic:
-            return [.heic]
-        case .html:
-            return [.html, .htm]
-        case .jpeg:
-            return [.jpeg, .jpg]
-        case .markdown:
-            return [.md, .markdown]
-        case .quickTimeVideo:
-            return [.mov]
-        case .tiff:
-            return [.tiff, .html]
-        }
-    }
-
-    // Construct a lookup table from extension to type.
-    // This is designed to crash if we don't provide a mapping for all file extensions or there are duplicate mappings.
-    private static let extensionToMIMEType: [String: String] = {
-        let mapping = FileType.allCases.reduce(into: [String: String]()) { partialResult, fileType in
-            for filenameExtension in extensions(for: fileType) {
-                precondition(partialResult[filenameExtension.rawValue] == nil)
-                partialResult[filenameExtension.rawValue] = fileType.rawValue
-            }
-        }
-        precondition(Set(mapping.keys) == Set(FilenameExtension.allCases.map({ $0.rawValue })))
-        return mapping
-    }()
-
-    // Construct a lookup table from type to extension.
-    // This is designed to crash if we don't provide at least one mapping for each file type.
-    private static let extensionToPreferredFilename: [String: String] = {
-        return FileType.allCases.reduce(into: [String: String]()) { partialResult, fileType in
-            partialResult[fileType.rawValue] = extensions(for: fileType).first!.rawValue
-        }
-    }()
-
-    var preferredFilenameExtension: String? {
-        return Self.extensionToPreferredFilename[self.rawValue]
-    }
-
-    var preferredMIMEType: String? {
-        return self.rawValue
-    }
-
-    init?(mimeType: String) {
-        self.init(rawValue: mimeType.lowercased())
-    }
-
-    init?(filenameExtension: String) {
-        guard let mimeType = Self.extensionToMIMEType[filenameExtension.lowercased()] else {
-            return nil
-        }
-        self.init(mimeType: mimeType)
-    }
-
-}
-
 class FileTypeTests: XCTestCase {
 
     func testFileTypeCaseSensitivity() {
@@ -176,7 +88,7 @@ class FileTypeTests: XCTestCase {
     }
 
     func testFilenameExtensions() {
-        for filenameExtension in FileType.FilenameExtension.allCases {
+        for filenameExtension in FileType.Extension.allCases {
             switch filenameExtension {
             case .gif:
                 XCTAssertEqual(FileType(filenameExtension: filenameExtension.rawValue), .gif)
@@ -201,6 +113,12 @@ class FileTypeTests: XCTestCase {
             }
         }
 
+    }
+
+    func testPreferredExtensionRoundTrip() {
+        for type in FileType.allCases {
+            XCTAssertEqual(FileType(filenameExtension: type.preferredFilenameExtension!), type)
+        }
     }
 
 }
